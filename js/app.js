8ede5080897b77138af4cce245a5166f188f6509
zeldown <3722389-zeldown@users.noreply.gitlab.com>
2021-05-29 14:23:59 +0200

    big update: upgrade emc-core to 1.2.1 & add a lot of options

--- js/app.js
+++ js/app.js
@@ -1,28 +1,48 @@
 const { ipcRenderer } = require('electron')
 
+function onload() {
+    ipcRenderer.send("getLauncherOpts");
+    document.querySelector('.ram-min').onchange = function() {
+        document.querySelector('.ram-min-label').innerHTML = "Ram minimum (" + this.value + "G)";
+    }
+    
+    document.querySelector('.ram-max').onchange = function() {
+        document.querySelector('.ram-max-label').innerHTML = "Ram maximum (" + this.value + "G)";
+    }
+}
+
+ipcRenderer.on("getLauncherOpts", (event, user, password) => {
+    document.querySelector('.email-input').value = user;
+    document.querySelector('.password-input').value = password;
+})
+
 function play() {
     document.querySelector('.error').style.opacity = "0";
     let email = document.querySelector('.email-input').value;
     let password = document.querySelector('.password-input').value;
 
     if(email === "" || password === "") {
-        document.querySelector('.error').innerHTML = "Please fill in your login details";
+        document.querySelector('.error').innerHTML = "Please fill credentials";
         document.querySelector('.error').style.opacity = "1";
         return;
     }
 
     document.querySelector('.play').disabled = true;
     document.querySelector('.play').innerHTML = "Play <i class='fas fa-circle-notch fa-spin'></i>";
-    ipcRenderer.send("play", email, password);
+    ipcRenderer.send("play", email, password, document.querySelector('.check-file').checked, document.querySelector('.check-save').checked, document.querySelector('.ram-min').value, document.querySelector('.ram-max').value);
 }
 
 ipcRenderer.on("log", (event, log) => {
     console.log(log);
 })
 
-ipcRenderer.on("check", (event) => {
-    document.querySelector('.progress-bar').style.width = "100%";
-    document.querySelector('.progress-bar').innerHTML = "<b>Checking files</b>";
+ipcRenderer.on("check", (event, current, total) => {
+    let p = (current/total*100);
+    if(p > 100) {
+        p = 100;
+    }
+    document.querySelector('.progress-bar').style.width = p + "%";
+    document.querySelector('.progress-bar').innerHTML = "<b>Checking files " + parseInt(p) + "% (" + current + "/" + total + ")</b>";
 })
 
 ipcRenderer.on("download", (event, current, total) => {
@@ -31,12 +51,12 @@ ipcRenderer.on("download", (event, current, total) => {
         p = 100;
     }
     document.querySelector('.progress-bar').style.width = p + "%";
-    document.querySelector('.progress-bar').innerHTML = "<b>Downloading " + parseInt(p) + "%</b>";
+    document.querySelector('.progress-bar').innerHTML = "<b>Downloading " + parseInt(p) + "% (" + bytesToSize(total-current) + ")</b>";
 })
 
 ipcRenderer.on("launch", (event) => {
     document.querySelector('.progress-bar').style.backgroundColor = "#33944b";
-    document.querySelector('.progress-bar').innerHTML = "<b>Starting Minecraft</b>";
+    document.querySelector('.progress-bar').innerHTML = "<b>Starting game</b>";
 })
 
 ipcRenderer.on("error", (event) => {
@@ -45,8 +65,23 @@ ipcRenderer.on("error", (event) => {
 })
 
 ipcRenderer.on("error-login", (event) => {
-    document.querySelector('.error').innerHTML = "Your credentials are incorrect";
+    document.querySelector('.error').innerHTML = "Wrong credentials";
     document.querySelector('.error').style.opacity = "1";
     document.querySelector('.play').disabled = false;
     document.querySelector('.play').innerHTML = "Play";
 })
+
+ipcRenderer.on("close", (event) => {
+    document.querySelector('.error').innerHTML = "The game was closed";
+    document.querySelector('.error').style.opacity = "1";
+    document.querySelector('.play').disabled = false;
+    document.querySelector('.play').innerHTML = "Play";
+    document.querySelector('.progress-bar').style.width = 0;
+})
+
+function bytesToSize(bytes) {
+    var sizes = ['Bytes', 'Ko', 'Mo', 'Go', 'To'];
+    if (bytes == 0) return '0 Byte';
+    var i = parseInt(Math.floor(Math.log(bytes) / Math.log(1024)));
+    return Math.round(bytes / Math.pow(1024, i), 2) + ' ' + sizes[i];
+ }
--- main.js
+++ main.js
@@ -6,12 +6,16 @@ const { ipcMain } = require('electron');
 
 const { MCAuth, MCLaunch } = require('emc-core');
 
+const fs = require('fs');
+
+const rootFile = ".emc-core-quickstart"
+
 const launcher = new MCLaunch();
 
 function createWindow () {
   const mainWindow = new BrowserWindow({
     width: 600,
-    height: 500,
+    height: 550,
     webPreferences: {
       nodeIntegration: true
     }
@@ -34,49 +38,91 @@ app.on('window-all-closed', function () {
   if (process.platform !== 'darwin') app.quit()
 })
 
-ipcMain.on("play", (event, username, password) => {
+ipcMain.on("getLauncherOpts", (event) => {
+  if(fs.existsSync(process.env.APPDATA + "/" + rootFile + "/launcheropts.json")) {
+    let rawData = fs.readFileSync(process.env.APPDATA + "/" + rootFile + "/launcheropts.json")
+    let json = JSON.parse(rawData);
+    event.reply("getLauncherOpts", json['user'], json['password']);
+  }
+})
+
+ipcMain.on("play", (event, username, password, check, save, ramMin, ramMax) => {
   let authenticator;
 
   MCAuth.auth(username, password).then(user => {
     authenticator = user;
+
+    if(save) {
+      let toSave = '{"user":"' + username + '", "password":"' + password + '"}';
+      console.log(toSave);
+      let toSaveJson = JSON.parse(toSave);
+      let toSaveString = JSON.stringify(toSaveJson, null, 4);
+
+      if(!fs.existsSync(process.env.APPDATA + "/" + rootFile + "/launcheropts.json")) {
+        fs.mkdirSync(process.env.APPDATA + "/" + rootFile, {recursive: true})
+      }
+      fs.writeFileSync(process.env.APPDATA + "/" + rootFile + "/launcheropts.json", toSaveString);
+    }
     
     let opts = {
-      url: "http://zeldown.com/minecraft/emc-core-quick-start/",
+      url: "http://zeldown.com/emc-core/exemple/",
       overrides: {
         detached: false
       },
       authorization: authenticator,
-      root: process.env.APPDATA + "/.emc-core-quick-start",
-      version: "1.16.5",
+      root: process.env.APPDATA + "/" + rootFile,
+      version: "1.15.2",
+      forge: "1.15.2-forge-31.2.0",
+      checkFiles: check,
       memory: {
-          max: "6G",
-          min: "4G"
+          max: ramMax + "G",
+          min: ramMin + "G"
       }
     }
 
     launcher.launch(opts);
 
-    launcher.on('debug', (e) => event.reply("log", "[DEBUG]" + e));
-    launcher.on('data', (e) => event.reply("log", "[DATA]" + e));
+    launcher.on('debug', (e) => {
+      console.log("[DEBUG]" + e);
+      event.reply("log", "[DEBUG]" + e)
+    });
+    launcher.on('data', (e) => {
+      console.log("[DATA]" + e);
+      event.reply("log", "[DATA]" + e)
+    });
     launcher.on('error', (e) => {
+      console.log("[ERROR]" + e);
       event.reply("log", "[ERROR]" + e)
       event.reply("error");
     });
 
     launcher.on('verification-status', (e) => {
+      console.log("[VERIFICATION] " + e.name + " (" + e.current + "/" + e.total + ")");
       event.reply("log", "[VERIFICATION] " + e.name + " (" + e.current + "/" + e.total + ")");
-      event.reply("check");
+      event.reply("check", e.current, e.total);
     });
 
     launcher.on('download-status', (e) => {
+      console.log("[DOWNLOAD][" + e.type + "] " + e.name + " (" + e.downloadedBytes + "/" + e.bytesToDownload + ")");
       event.reply("log", "[DOWNLOAD][" + e.type + "] " + e.name + " (" + e.downloadedBytes + "/" + e.bytesToDownload + ")");
-      event.reply("download", e.downloadedBytes, e.bytesToDownload);
+      if(e.downloadedBytes > e.bytesToDownload) {
+        event.reply("download", e.downloadedBytes, e.downloadedBytes);
+      }else {
+        event.reply("download", e.downloadedBytes, e.bytesToDownload);
+      }
     });
 
     launcher.on('launch', (e) => {
+      console.log("Launching minecraft");
       event.reply("log", "Launching minecraft");
       event.reply("launch");
     });
+
+    launcher.on('close', (e) => {
+      console.log("Closing minecraft");
+      event.reply("log", "Closing minecraft");
+      event.reply("close");
+    })
   }).catch(error => {
     event.reply("log", error);
     event.reply("error-login");
